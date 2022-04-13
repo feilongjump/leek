a2751da8d99a2748593ce5ab46aa654fd48916ab
feilongjump <feilongjump@qq.com>
2022-04-13 21:50:07 +0800

    :construction: 导航栏

---
+++ src/layout/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="navbar-container">
+    <div>
+      <ul class="flex">
+        <li
+          v-for="(item, index) in 5"
+          :key="index"
+          class="w-8 h-8 rounded-full p-3 mr-3 bg-gray-200 first-line:text-indigo-400 first-line:font-bold text-white flex justify-center items-center"
+        >
+          {{ item }}
+        </li>
+      </ul>
+    </div>
+    <div class="flex">
+      <div>search</div>
+      <div>user</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#navbar-container {
+  @apply h-12 md:h-20 px-2 md:px-4 ml-0 md:ml-24 lg:ml-64 fixed
+    transition-all duration-500 ease-in-out
+    flex justify-between items-center;
+}
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,12 +1,18 @@
 <script lang="ts" setup>
 import Sidebar from './components/Sidebar.vue'
+import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
-  <div class="min-h-screen w-screen">
+  <div class="min-h-screen w-screen flex">
     <Sidebar />
 
-    <div id="backstage-container" class="ml-64">
+    <Navbar />
+
+    <div
+      id="backstage-container"
+      class="ml-0 md:ml-24 lg:ml-64 mt-12 md:mt-20 pt-4 px-2 md:px-4 transition-all duration-500 ease-in-out w-full"
+    >
       <router-view></router-view>
     </div>
   </div>
