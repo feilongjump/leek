35e6f0f5015c3d4db6e6ab09a0fd94de43ea23d0
feilongjump <feilongjump@qq.com>
2022-04-14 16:04:52 +0800

    :sparkles: 导航栏

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,27 +1,88 @@
+<script setup lang="ts">
+import avatar from '@/assets/avatar.jpg'
+import {
+  BeakerIcon,
+  ArchiveIcon,
+  ChartPieIcon,
+  ColorSwatchIcon,
+  FireIcon,
+  BellIcon,
+  ChatIcon
+} from '@heroicons/vue/outline'
+
+const menus = [
+  {
+    icon: BeakerIcon,
+    bgColor: 'bg-indigo-100',
+    strokeColor: 'stroke-indigo-400'
+  },
+  {
+    icon: ArchiveIcon,
+    bgColor: 'bg-gray-100',
+    strokeColor: 'stroke-gray-400'
+  },
+  {
+    icon: ChartPieIcon,
+    bgColor: 'bg-gray-100',
+    strokeColor: 'stroke-gray-400'
+  },
+  {
+    icon: ColorSwatchIcon,
+    bgColor: 'bg-gray-100',
+    strokeColor: 'stroke-gray-400'
+  },
+  {
+    icon: FireIcon,
+    bgColor: 'bg-gray-100',
+    strokeColor: 'stroke-gray-400'
+  }
+]
+</script>
+
 <template>
   <div id="navbar-container">
-    <div>
+    <div class="w-full md:w-1/3 lg:max-w-sm">
       <ul class="flex">
         <li
-          v-for="(item, index) in 5"
+          v-for="(item, index) in menus"
           :key="index"
-          class="w-8 h-8 rounded-full p-3 mr-3 bg-gray-200 first-line:text-indigo-400 first-line:font-bold text-white flex justify-center items-center"
+          class="w-8 h-8 rounded-full p-1 mr-6 text-white flex justify-center items-center"
+          :class="item.bgColor"
         >
-          {{ item }}
+          <component :is="item.icon" class="h-6 w-6 stroke-2" :class="item.strokeColor"></component>
         </li>
       </ul>
     </div>
-    <div class="flex">
-      <div>search</div>
-      <div>user</div>
+    <div class="hidden md:flex items-center">
+      <div class="mr-6">
+        <input
+          class="pl-2 border-b-2 outline-none transition ease-in-out duration-300 focus:border-indigo-500"
+          placeholder="Search ..."
+        />
+      </div>
+      <div class="flex items-center">
+        <BellIcon class="h-6 w-6 stroke-blue-400 mr-6" />
+        <ChatIcon class="h-6 w-6 stroke-indigo-400 mr-6" />
+        <img class="h-8 w-8 rounded-full" :src="avatar" />
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped>
 #navbar-container {
-  @apply h-12 md:h-20 px-2 md:px-4 ml-0 md:ml-24 lg:ml-64 fixed
+  @apply h-12 w-screen md:h-20 px-2 md:px-4 ml-0 md:ml-24 lg:ml-64 fixed
     transition-all duration-500 ease-in-out
     flex justify-between items-center;
+}
+@screen md {
+  #navbar-container {
+    width: calc(theme('width.screen') - theme('width.24'));
+  }
+}
+@screen lg {
+  #navbar-container {
+    width: calc(theme('width.screen') - theme('width.64'));
+  }
 }
 </style>
