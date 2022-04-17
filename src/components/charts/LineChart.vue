815ff841d107db4d19cef12b0fc1d5dfc66e47f1
feilongjump <feilongjump@qq.com>
2022-04-17 22:12:38 +0800

    折线图

---
+++ src/components/charts/LineChart.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+
+import * as echarts from 'echarts/core'
+import { GridComponent } from 'echarts/components'
+import { LineChart, LineSeriesOption } from 'echarts/charts'
+import { UniversalTransition } from 'echarts/features'
+import { CanvasRenderer } from 'echarts/renderers'
+
+echarts.use([GridComponent, LineChart, CanvasRenderer, UniversalTransition])
+
+type ECOption = echarts.ComposeOption<LineSeriesOption>
+
+interface Props {
+  options: LineSeriesOption
+}
+
+const lineChart = ref<HTMLElement>()
+const props = defineProps<Props>()
+
+onMounted(() => {
+  // 创建图表
+  const chartInstance = echarts.init(lineChart.value as HTMLElement)
+  chartInstance.setOption(props.options as ECOption)
+})
+</script>
+
+<template>
+  <div ref="lineChart"></div>
+</template>
--- src/views/backstage/dashboard/index.vue
+++ src/views/backstage/dashboard/index.vue
@@ -1,3 +1,82 @@
+<script setup lang="ts">
+import LineChartVue from '@/components/charts/LineChart.vue'
+
+const options = {
+  xAxis: {
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    boundaryGap: false,
+    axisTick: {
+      show: false
+    }
+  },
+  grid: {
+    left: 10,
+    right: 10,
+    bottom: 20,
+    top: 30,
+    containLabel: true
+  },
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross'
+    },
+    padding: [5, 10]
+  },
+  yAxis: {
+    axisTick: {
+      show: false
+    }
+  },
+  legend: {
+    data: ['expected', 'actual']
+  },
+  series: [
+    {
+      name: 'expected',
+      itemStyle: {
+        normal: {
+          color: '#FF005A',
+          lineStyle: {
+            color: '#FF005A',
+            width: 2
+          }
+        }
+      },
+      smooth: true,
+      type: 'line',
+      data: [100, 120, 161, 134, 105, 160, 165],
+      animationDuration: 2800,
+      animationEasing: 'cubicInOut'
+    },
+    {
+      name: 'actual',
+      smooth: true,
+      type: 'line',
+      itemStyle: {
+        normal: {
+          color: '#3888fa',
+          lineStyle: {
+            color: '#3888fa',
+            width: 2
+          },
+          areaStyle: {
+            color: '#f3f8ff'
+          }
+        }
+      },
+      data: [120, 82, 91, 154, 162, 140, 145],
+      animationDuration: 2800,
+      animationEasing: 'quadraticOut'
+    }
+  ]
+}
+</script>
+
 <template>
-  <span class="text-3xl font-bold">Backstage Dashboard</span>
+  <span class="text-2xl font-bold">Dashboard</span>
+
+  <div class="w-full mt-6 flex">
+    <LineChartVue class="w-full h-96 rounded-xl shadow-xl bg-white" :options="options" />
+  </div>
 </template>
