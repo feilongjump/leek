9f7ab0afb263ab47052f6ffb23d51524b9716582
feilongjump <feilongjump@qq.com>
2022-04-21 10:59:08 +0800

    使用组件加载方式，加载折线图和日历

---
+++ src/views/backstage/dashboard/components/LineChart.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+import LineChart from '@/components/charts/LineChart.vue'
+
+const options = {
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {
+      type: 'cross'
+    },
+    padding: [5, 10]
+  },
+  grid: {
+    top: '2%',
+    left: '2%',
+    right: '2%',
+    bottom: '8%',
+    containLabel: true
+  },
+  legend: {
+    bottom: '0%',
+    data: ['expected', 'actual']
+  },
+  xAxis: {
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    boundaryGap: false
+  },
+  yAxis: {},
+  series: [
+    {
+      name: 'expected',
+      smooth: true,
+      type: 'line',
+      itemStyle: {
+        normal: {
+          color: '#FF005A',
+          lineStyle: {
+            color: '#FF005A',
+            width: 3
+          }
+        }
+      },
+      data: [556, 725, 424, 356, 586, 756, 616],
+      animationDuration: 2800,
+      animationEasing: 'cubicInOut'
+    },
+    {
+      name: 'actual',
+      smooth: true,
+      type: 'line',
+      itemStyle: {
+        normal: {
+          color: '#3888fa',
+          lineStyle: {
+            color: '#3888fa',
+            width: 3
+          },
+          areaStyle: {
+            color: '#f3f8ff'
+          }
+        }
+      },
+      data: [356, 218, 156, 526, 356, 256, 56],
+      animationDuration: 2800,
+      animationEasing: 'quadraticOut'
+    }
+  ]
+}
+</script>
+
+<template>
+  <span class="w-full mb-4 text-2xl">Title</span>
+  <LineChart class="w-full h-96" :options="options" />
+  <div class="w-full mt-4 pt-6 border-t border-gray-200 flex justify-between">
+    <span class="text-gray-400 cursor-pointer">Options</span>
+    <span class="text-indigo-400 cursor-pointer">Link</span>
+  </div>
+</template>
--- src/views/backstage/dashboard/index.vue
+++ src/views/backstage/dashboard/index.vue
@@ -1,81 +1,39 @@
 <script setup lang="ts">
-import LineChartVue from '@/components/charts/LineChart.vue'
-
-const options = {
-  tooltip: {
-    trigger: 'axis',
-    axisPointer: {
-      type: 'cross'
-    },
-    padding: [5, 10]
-  },
-  grid: {
-    top: '2%',
-    left: '2%',
-    right: '2%',
-    bottom: '8%',
-    containLabel: true
-  },
-  legend: {
-    bottom: '0%',
-    data: ['expected', 'actual']
-  },
-  xAxis: {
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    boundaryGap: false
-  },
-  yAxis: {},
-  series: [
-    {
-      name: 'expected',
-      smooth: true,
-      type: 'line',
-      itemStyle: {
-        normal: {
-          color: '#FF005A',
-          lineStyle: {
-            color: '#FF005A',
-            width: 3
-          }
-        }
-      },
-      data: [556, 725, 424, 356, 586, 756, 616],
-      animationDuration: 2800,
-      animationEasing: 'cubicInOut'
-    },
-    {
-      name: 'actual',
-      smooth: true,
-      type: 'line',
-      itemStyle: {
-        normal: {
-          color: '#3888fa',
-          lineStyle: {
-            color: '#3888fa',
-            width: 3
-          },
-          areaStyle: {
-            color: '#f3f8ff'
-          }
-        }
-      },
-      data: [356, 218, 156, 526, 356, 256, 56],
-      animationDuration: 2800,
-      animationEasing: 'quadraticOut'
-    }
-  ]
-}
+import LineChart from './components/LineChart.vue'
+import Calendar from './components/Calendar.vue'
 </script>
 
 <template>
   <span class="text-2xl font-bold">Dashboard</span>
 
-  <div class="w-full xl:w-2/3 mt-6 p-6 flex flex-col rounded-xl shadow-xl bg-white">
-    <span class="w-full mb-4 text-2xl">Title</span>
-    <LineChartVue class="w-full h-96" :options="options" />
-    <div class="w-full mt-4 pt-6 border-t border-gray-200 flex justify-between">
-      <span class="text-gray-400 cursor-pointer">Options</span>
-      <span class="text-indigo-400 cursor-pointer">Link</span>
+  <div class="w-full mt-6 flex flex-wrap">
+    <div class="line-chart-dom">
+      <LineChart />
+    </div>
+
+    <div class="w-full 2xl:w-96 flex flex-wrap md:flex-nowrap 2xl:block">
+      <div
+        class="w-full md:w-1/2 2xl:w-full h-[28rem] p-6 mt-6 md:mr-6 2xl:mt-0 2xl:mr-0 bg-white rounded-xl shadow-xl"
+      >
+        <Calendar />
+      </div>
+      <div class="w-full md:w-1/2 2xl:w-full h-80 bg-white mt-6 rounded-xl shadow-xl">
+        <div class="w-full h-full flex justify-center items-center text-2xl">
+          <span>reserve</span>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<style>
+.line-chart-dom {
+  @apply w-full p-6 2xl:mr-6 flex flex-col rounded-xl shadow-xl bg-white;
+}
+
+@screen 2xl {
+  .line-chart-dom {
+    width: calc(theme('width.full') - theme('width.96') - theme('margin.6'));
+  }
+}
+</style>
