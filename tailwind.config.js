498dc2b3a97048462d13be75c25375bceda58832
feilongjump <feilongjump@qq.com>
2022-04-28 17:42:49 +0800

    check svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ const scrollbar = require('@gradin/tailwindcss-scrollbar')
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        check: "url('/src/assets/check.svg')"
+      }
+    },
     scrollbar: () => ({
       DEFAULT: {
         size: '0px'
