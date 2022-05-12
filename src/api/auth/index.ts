063277d26adbf43f122aae5c9ef1a8b455471a79
feilongjump <feilongjump@qq.com>
2022-05-12 21:46:06 +0800

    更改参数传递

--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -4,9 +4,9 @@ import { LoginParams, LoginResponse } from './types'
 export default class Auth extends Request {
   BASE_URL: string = ''
 
-  public login(params: LoginParams): Promise<LoginResponse> {
+  public login(data: LoginParams): Promise<LoginResponse> {
     const url = `${this.BASE_URL}/login`
 
-    return super.post(url, params)
+    return super.post(url, '', data)
   }
 }
