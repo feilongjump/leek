e4f99cb68be8a4e4b08012d43ad2b7df71c6614a
feilongjump <feilongjump@qq.com>
2022-06-01 07:14:20 +0800

    :fire: remove code

--- src/views/backstage/project/detail.vue
+++ src/views/backstage/project/detail.vue
@@ -7,11 +7,5 @@
       <span class="mx-2">/</span>
       <span class="">Detail</span>
     </div>
-    <router-link
-      :to="{ name: 'Backstage.Project.Add' }"
-      class="flex justify-center items-center bg-indigo-400 text-sm text-white rounded-lg px-4 py-2 hover:bg-indigo-500"
-    >
-      <DocumentAddIcon class="w-5 h-5 mr-1" />Add
-    </router-link>
   </div>
 </template>
