024dc77a3f1d7ba96e0cab3d9025d0bfdf247fc7
feilongjump <feilongjump@qq.com>
2022-06-01 07:15:03 +0800

    :bug: 获取 Project 详情数据

--- src/views/backstage/project/components/Detail.vue
+++ src/views/backstage/project/components/Detail.vue
@@ -2,10 +2,10 @@
   <div class="w-screen h-screen bg-white fixed top-0 left-0">
     <div class="h-14 w-full px-8 border-b border-gray-200 flex justify-between items-center">
       <input
-        v-model="article.title"
+        v-model="project.name"
         type="text"
         class="title-input h-full text-3xl outline-none placeholder:text-xl placeholder:align-middle"
-        placeholder="Give me a title !"
+        placeholder="Give me a Name !"
       />
       <div class="w-64 h-full flex justify-between items-center">
         <button class="w-16 hollow-indigo-btn">草稿</button>
@@ -18,7 +18,7 @@
         </router-link>
       </div>
     </div>
-    <Editor v-model:value="article.content.markdown" />
+    <Editor v-model:value="project.content.markdown" />
   </div>
 </template>
 
@@ -27,13 +27,13 @@ import { ReplyIcon } from '@heroicons/vue/outline'
 import Editor from '@/components/Markdown/Editor.vue'
 import { onMounted, ref } from 'vue'
 import router from '@/router'
-import Article from '@/api/article/index'
-import { ArticleResponse } from '@/api/article/types'
+import Project from '@/api/project/index'
+import { ProjectResponse } from '@/api/project/types'
 import { ElMessage } from 'element-plus'
 
-const article = ref<ArticleResponse>({
+const project = ref<ProjectResponse>({
   id: 0,
-  title: '',
+  name: '',
   created_at: '',
   updated_at: '',
   content: {
@@ -48,27 +48,27 @@ const props = defineProps({
     default: false
   }
 })
-const ArticleRequest = new Article()
+const ProjectRequest = new Project()
 
 onMounted(() => {
   if (props.isEdit) {
     const { id } = router.currentRoute.value.params
-    ArticleRequest.show(id).then((response) => {
-      article.value = response
+    ProjectRequest.show(id).then((response) => {
+      project.value = response
     })
   }
 })
 
 const publish = () => {
   if (props.isEdit) {
-    ArticleRequest.update(article.value.id, article.value).then(() => {
+    ProjectRequest.update(project.value.id, project.value).then(() => {
       ElMessage.success('修改成功')
     })
   } else {
-    ArticleRequest.store(article.value).then(() => {
+    ProjectRequest.store(project.value).then(() => {
       ElMessage.success('发布成功')
 
-      router.push({ name: 'Backstage.Article' })
+      router.push({ name: 'Backstage.Project' })
     })
   }
 }
